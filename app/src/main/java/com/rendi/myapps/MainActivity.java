634dab228df3dbeffdcadc25a0d09599a78149b3
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2019-07-24 17:32:40 +0700

    Initial commit

---
+++ app/src/main/java/com/rendi/myapps/MainActivity.java
@@ -0,0 +1,35 @@
+package com.rendi.myapps;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.rendi.myapps.adapter.ListHeroAdapter;
+import com.rendi.myapps.model.Hero;
+import com.rendi.myapps.model.HeroesData;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_hero);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+    }
+    private void showRecyclerList(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+        }
+    }
---
+++ app/src/main/java/com/rendi/myapps/model/Hero.java
@@ -0,0 +1,31 @@
+package com.rendi.myapps.model;
+
+public class Hero {
+    private String heroes_name;
+    private String from;
+    private String photo;
+
+    public String getHeroes_name() {
+        return heroes_name;
+    }
+
+    public void setHeroes_name(String heroes_name) {
+        this.heroes_name = heroes_name;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
